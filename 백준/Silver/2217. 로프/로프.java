2942b02eb3ce860f05cc8eb9ab2d4a215161f213
정콩 <56215526+Jeongbin233@users.noreply.github.com>
2023-04-02 00:00:29 +0900

    [Silver IV] Title: 로프, Time: 752 ms, Memory: 89108 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int max = 0;
+        for(int i=0; i<n; i++){
+            max = Math.max(max, (n-i)*arr[i]);
+        }
+        System.out.println(max);
+
+    }//main
+}
