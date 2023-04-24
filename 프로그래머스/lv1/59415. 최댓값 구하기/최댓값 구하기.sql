8196477651fba68c061d4eaaa86c63c337a9e423
정콩 <56215526+Jeongbin233@users.noreply.github.com>
2023-04-24 23:43:06 +0900

    [level 1] Title: 최댓값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59415. 최댓값 구하기/최댓값 구하기.sql
+++ 프로그래머스/lv1/59415. 최댓값 구하기/최댓값 구하기.sql
@@ -1,5 +1,8 @@
 -- 코드를 입력하세요
-SELECT datetime as 시간
-from animal_ins
-order by datetime desc
-limit 1;
+# SELECT datetime as 시간
+# from animal_ins
+# order by datetime desc
+# limit 1;
+
+select MAX(datetime)
+from animal_ins;
