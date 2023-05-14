4488eb1e3c0c9610a32750034a8b1bc424cac4ab
정콩 <56215526+Jeongbin233@users.noreply.github.com>
2023-05-14 22:28:03 +0900

    [Bronze V] Title: 웰컴, Time: 76 ms, Memory: 11276 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        /*
+.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.
+         */
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+
+    }
+}
