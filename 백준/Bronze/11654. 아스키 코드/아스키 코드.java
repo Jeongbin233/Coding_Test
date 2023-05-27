ba060cb31fc52fe14374b78c8b750e831b371b9c
정콩 <56215526+Jeongbin233@users.noreply.github.com>
2023-05-27 22:43:26 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 12792 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println((int)str.charAt(0));
+    }
+}
