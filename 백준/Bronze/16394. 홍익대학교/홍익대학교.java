aecba5cc3ae6fdf883c58166821cfd7b606c7c71
정콩 <56215526+Jeongbin233@users.noreply.github.com>
2023-06-17 08:25:14 +0900

    [Bronze V] Title: 홍익대학교, Time: 112 ms, Memory: 12888 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(year-1946);
+    }//main
+}
