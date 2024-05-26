338a57cb1ae65c10a2a5d1eea10abb6a20e87b19
정콩 <56215526+Jeongbin233@users.noreply.github.com>
2024-05-27 00:49:06 +0900

    [Silver II] Title: 촌수계산, Time: 80 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,15 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n, m, num1, num2;
-    static int[][] family = new int[101][101];
-    static int[] visited = new int[101];
-    public static void main(String[] args) throws Exception{
+    static int res = -1;
+    static boolean flag = false;
+    static List<Integer>[] family;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
+        family = new ArrayList[n+1];
+        for(int i=1; i<=n; i++){
+            family[i] = new ArrayList<>();
+        }
+        visited = new boolean[n+1];
 
         String str = br.readLine();
         st = new StringTokenizer(str);
@@ -18,39 +28,30 @@ public class Main {
         num2 = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<m; i++){
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            family[x][y] = 1;
-            family[y][x] = 1;
+            family[x].add(y);
+            family[y].add(x);
         }//for
 
-        bfs();
-        if(visited[num2] > 0)
-            System.out.println(visited[num2] -1);
-        else
-            System.out.println(-1);
-
-
-    }//main
-
-
-    public static void bfs(){
-        Deque<Integer> q = new ArrayDeque<>();
-        q.push(num1);
-        visited[num1] = 1;
-        while(!q.isEmpty()){
-            int person = q.poll();
-            int tmp = visited[person];
-            for(int i=1; i<=n; i++){
-                if(family[person][i]>0 && visited[i]<=0){
-                    q.push(i);
-                    visited[i] = tmp + 1;
-                }
-            }
+        dfs(num1, num2, 0);
+        System.out.println(res);
+    }
+    public static void dfs(int a, int b, int cnt){
+        if(a == b){
+            res = cnt;
+            return;
         }
 
-    }//bfs
+        visited[a] = true;
+        for(int i=0; i<family[a].size(); i++){
+            int next = family[a].get(i);
+            if(!visited[next]){
+                dfs(next, b, cnt+1);
+            }
+        }
+    }//dfs
 }
