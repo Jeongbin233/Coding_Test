61d5c794fe57b133955cb85c6242151b2dccde09
정콩 <56215526+Jeongbin233@users.noreply.github.com>
2024-09-21 19:51:22 +0900

    Time: 305 ms (49.68%), Space: 62.7 MB (41.30%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,33 +1,79 @@
 class Solution {
+      class Edge implements Comparable<Edge> {
+        int src, dest, weight;
+        
+        Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+        
+        @Override
+        public int compareTo(Edge other) {
+            return this.weight - other.weight;
+        }
+    }
+    
+    class UnionFind {
+        int[] parent;
+        
+        UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+        
+        int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+        
+        void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                parent[rootX] = rootY;
+            }
+        }
+    }
+    
     public int minCostConnectPoints(int[][] points) {
-       PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]); // edge weight, the index of next node
-        pq.offer(new int[] { 0, 0 });
-        int len = points.length;
-        Set<Integer> visited = new HashSet<>();
+        int n = points.length;
+        List<Edge> edges = new ArrayList<>();
+        
+        // 모든 가능한 간선 생성
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int weight = Math.abs(points[i][0] - points[j][0]) + 
+                             Math.abs(points[i][1] - points[j][1]);
+                edges.add(new Edge(i, j, weight));
+            }
+        }
+        
+        // 간선을 가중치 기준으로 정렬
+        Collections.sort(edges);
+        
+        UnionFind uf = new UnionFind(n);
         int cost = 0;
-
-        // When visited.size() == points.len meaning that all the nodes has been connected.
-        while (visited.size() < len) {
-            int[] arr = pq.poll();
-
-            int weight = arr[0];
-            int currNode = arr[1];
-
-            if (visited.contains(currNode)) continue;
-
-            visited.add(currNode);
-            cost += weight;
-
-            for (int nextNode = 0; nextNode < len; nextNode++) {
-                if (!visited.contains(nextNode)) {
-                    int nextWeight =
-                        Math.abs(points[nextNode][0] - points[currNode][0]) +
-                        Math.abs(points[nextNode][1] - points[currNode][1]);
-                    pq.offer(new int[] { nextWeight, nextNode });
+        int edgesUsed = 0;
+        
+        // 크루스칼 알고리즘 수행
+        for (Edge edge : edges) {
+            if (uf.find(edge.src) != uf.find(edge.dest)) {
+                uf.union(edge.src, edge.dest);
+                cost += edge.weight;
+                edgesUsed++;
+                
+                // n-1개의 간선을 선택하면 모든 점이 연결됨
+                if (edgesUsed == n - 1) {
+                    break;
                 }
             }
         }
-
+        
         return cost;
     }
 }
