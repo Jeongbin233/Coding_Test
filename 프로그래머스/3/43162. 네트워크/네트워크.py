3003affb053e5ca30aac633049b4b2d75cb5148f
정콩 <56215526+Jeongbin233@users.noreply.github.com>
2025-07-30 23:28:23 +0900

    [level 3] Title: 네트워크, Time: 0.90 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,20 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+    
+    def dfs(x):
+        visited[x] = True
+    
+        for i in range(n):
+            if computers[x][i] == 1 and x != i:
+                if not visited[i]:
+                    dfs(i)
+
+    
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            answer += 1
+    
+    return answer
+    
